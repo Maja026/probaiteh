31c552d678a6af15844f66f91d458985d540b1dc
Maja Saponjic <ms20210338@student.fon.bg.ac.rs>
2024-12-10 19:45:16 +0100

    Dodata autentifikacija profesora i korisnika kursa

--- proba/routes/api.php
+++ proba/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KursController;
+use App\Http\Controllers\AuthController;
 
 // Ruta za autentifikaciju korisnika
 Route::get('/korisnik', function (Request $request) {
@@ -18,9 +19,21 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('kursevi/{id}', [KursController::class, 'destroy'])->name('kursevi.destroy');
 });
 
+// Registracija i prijava korisnika
+Route::post('register/korisnik', [AuthController::class, 'registerKorisnik']);
+Route::post('login/korisnik', [AuthController::class, 'loginKorisnik']);
+
+// Registracija i prijava profesora
+Route::post('register/profesor', [AuthController::class, 'registerProfesor']);
+Route::post('login/profesor', [AuthController::class, 'loginProfesor']);
+
+// Logout
+Route::post('logout', [AuthController::class, 'logout']);
+
 // Handle errors in JSON format
 Route::fallback(function () {
     return response()->json([
         'error' => 'Not Found'
     ], 404);
 });
+
